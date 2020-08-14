2d06cddc06a655fc750959ca5e044a9be9898164
Gabriel R <grxgabriel@gmail.com>
2020-08-13 23:25:25 -0300

    :art: add prettier, remove package-lock and normalize some texts

--- src/app.js
+++ src/app.js
@@ -1,155 +1,174 @@
-import React from 'react';
+import React from 'react'
 
 import {
-    Input, Textarea, Button,
-    Navbar, NavItem, Select,
-    SelectOption, Card, Table,
-    TableRow, Footer, FooterBottom,
-    Modal,
-} from '../src/lib/atomix';
+  Input,
+  Textarea,
+  Button,
+  Navbar,
+  NavItem,
+  Select,
+  SelectOption,
+  Card,
+  Table,
+  TableRow,
+  Footer,
+  FooterBottom,
+  Modal,
+} from '../src/lib/atomix'
 
-export default props => {
+export default (props) => {
+  return (
+    <div>
+      <Navbar
+        brand="Atomix"
+        icon="https://static1.squarespace.com/static/54e4ede1e4b012fa4a38638c/t/5b96a82a0e2e72baf585cc78/1536600122199/Atom.png"
+      >
+        <NavItem
+          link="http://www.facebook.com"
+          blankTarget={true}
+          text="Facebook"
+        />
+        <NavItem
+          link="http://www.linkedin.com"
+          blankTarget={true}
+          text="Linkedin"
+        />
+        <NavItem
+          link="http://www.github.com"
+          blankTarget={true}
+          text="github"
+        />
+      </Navbar>
 
-    return (
+      <div className="container">
+        <div className="container">
+          <Modal
+            name="firstModal"
+            text="Botão da Modal"
+            style="primary"
+            title="Titulo"
+            content="Olá, eu sou uma modal linda pra caramba =D"
+          />
+
+          <Modal
+            name="secondaModal"
+            text="Botão da Modal 2"
+            style="primary"
+            title="Titulo"
+            content="Mensagem da modal"
+          />
+        </div>
+
+        <div className="vertical-content">
+          <Input type="password" placeholder="Placeholder" value="Value" />
+
+          <Input type="number" placeholder="Placeholder" value="Value" />
+        </div>
+
+        <Button
+          style="primary"
+          onClick={() => alert('Botão acionado com sucesso')}
+          text="Teste"
+        />
+        <Button
+          style="primary"
+          onClick={() => alert('Botão acionado com sucesso')}
+          text="Teste"
+        />
+        <Button
+          style="warning"
+          onClick={() => alert('Botão acionado com sucesso')}
+          text="Teste"
+        />
+        <Button
+          onClick={() => alert('Botão acionado com sucesso')}
+          text="Teste"
+        />
+
         <div>
-            <Navbar brand='Atomix' icon='https://static1.squarespace.com/static/54e4ede1e4b012fa4a38638c/t/5b96a82a0e2e72baf585cc78/1536600122199/Atom.png'>
-                <NavItem link='http://www.facebook.com' blankTarget={true} text='Facebook' />
-                <NavItem link='http://www.linkedin.com' blankTarget={true} text='Linkedin' />
-                <NavItem link='http://www.github.com' blankTarget={true} text='github' />
-            </Navbar>
+          <Textarea type="text" placeholder="Placeholder" value="Value" />
+        </div>
 
+        <Select>
+          <SelectOption value="Valor" text="Texto" />
+        </Select>
 
-            <div className='container'>
+        <div className="content horizontal-content">
+          <Card
+            img="https://conteudo.imguol.com.br/c/entretenimento/c4/2018/05/15/super-mario-odyssey-1526426783086_v2_1170x540.jpgx"
+            title="Título"
+            subtitle="Subtitle"
+            content="Eu sou um content, estou sendo testado. Eu sou um content, estou sendo testado. Eu sou um content, estou sendo testado. "
+            footer="Meu footer"
+          />
+          <Card
+            img="https://conteudo.imguol.com.br/c/entretenimento/c4/2018/05/15/super-mario-odyssey-1526426783086_v2_1170x540.jpgx"
+            title="Título"
+            subtitle="Subtitle"
+            content="Eu sou um content, estou sendo testado. Eu sou um content, estou sendo testado. Eu sou um content, estou sendo testado. "
+            footer="Meu footer"
+          />
+          <Card
+            img="https://conteudo.imguol.com.br/c/entretenimento/c4/2018/05/15/super-mario-odyssey-1526426783086_v2_1170x540.jpgx"
+            title="Título"
+            subtitle="Subtitle"
+            content="Eu sou um content, estou sendo testado. Eu sou um content, estou sendo testado. Eu sou um content, estou sendo testado. "
+            footer="Meu footer"
+          />
+          <Card
+            img="https://conteudo.imguol.com.br/c/entretenimento/c4/2018/05/15/super-mario-odyssey-1526426783086_v2_1170x540.jpgx"
+            title="Título"
+            subtitle="Subtitle"
+            content="Eu sou um content, estou sendo testado. Eu sou um content, estou sendo testado. Eu sou um content, estou sendo testado. "
+            footer="Meu footer"
+          />
+          <Card
+            img="https://conteudo.imguol.com.br/c/entretenimento/c4/2018/05/15/super-mario-odyssey-1526426783086_v2_1170x540.jpgx"
+            title="Título"
+            subtitle="Subtitle"
+            content="Eu sou um content, estou sendo testado. Eu sou um content, estou sendo testado. Eu sou um content, estou sendo testado. "
+            footer="Meu footer"
+          />
+          <Card
+            img="https://conteudo.imguol.com.br/c/entretenimento/c4/2018/05/15/super-mario-odyssey-1526426783086_v2_1170x540.jpgx"
+            title="Título"
+            subtitle="Subtitle"
+            content="Eu sou um content, estou sendo testado. Eu sou um content, estou sendo testado. Eu sou um content, estou sendo testado. "
+            footer="Meu footer"
+          />
+          <Card
+            img="https://conteudo.imguol.com.br/c/entretenimento/c4/2018/05/15/super-mario-odyssey-1526426783086_v2_1170x540.jpgx"
+            title="Título"
+            subtitle="Subtitle"
+            content="Eu sou um content, estou sendo testado. Eu sou um content, estou sendo testado. Eu sou um content, estou sendo testado. "
+            footer="Meu footer"
+          />
+        </div>
 
-                <div className='container'>
+        <Table
+          headers={['FirstName', 'Second Name', 'Third Name', 'Random', 'Food']}
+        >
+          <TableRow values={['Polenta', 'com', 'ceva', 'mais', 'tarde?']} />
+          <TableRow values={['4', '5', '7', '123', 'batata']} />
+        </Table>
+      </div>
 
-                    <Modal
-                        name='firstModal'
-                        text='Botão da Modal'
-                        style='primary'
-                        title='Titulo'
-                        content='Olá, eu sou uma modal linda pra caramba =D'
-                    />
-
-                    <Modal
-                        name='secondaModal'
-                        text='Botão da Modal 2'
-                        style='primary'
-                        title='Titulo'
-                        content='Mensagem da modal'
-                    />
-                </div>
-
-                <div className='vertical-content'>
-                    <Input
-                        type='password'
-                        placeholder='Placeholder'
-                        value='Value'
-                    />
-
-                    <Input
-                        type='number'
-                        placeholder='Placeholder'
-                        value='Value'
-                    />
-                </div>
-
-                <Button style='primary' onClick={() => alert('Botão acionado com sucesso')} text='Teste' />
-                <Button style='primary' onClick={() => alert('Botão acionado com sucesso')} text='Teste' />
-                <Button style='warning' onClick={() => alert('Botão acionado com sucesso')} text='Teste' />
-                <Button onClick={() => alert('Botão acionado com sucesso')} text='Teste' />
-
-                <div>
-                    <Textarea
-                        type='text'
-                        placeholder='Placeholder'
-                        value='Value'
-                    />
-                </div>
-
-                <Select>
-                    <SelectOption value='Valor' text='Texto' />
-                </Select>
-
-                <div className='content horizontal-content'>
-                    <Card
-                        img='https://conteudo.imguol.com.br/c/entretenimento/c4/2018/05/15/super-mario-odyssey-1526426783086_v2_1170x540.jpgx'
-                        title='Título'
-                        subtitle='Subtitle'
-                        content='Eu sou um content, estou sendo testado. Eu sou um content, estou sendo testado. Eu sou um content, estou sendo testado. '
-                        footer='Meu footer'
-                    />
-                    <Card
-                        img='https://conteudo.imguol.com.br/c/entretenimento/c4/2018/05/15/super-mario-odyssey-1526426783086_v2_1170x540.jpgx'
-                        title='Título'
-                        subtitle='Subtitle'
-                        content='Eu sou um content, estou sendo testado. Eu sou um content, estou sendo testado. Eu sou um content, estou sendo testado. '
-                        footer='Meu footer'
-                    />
-                    <Card
-                        img='https://conteudo.imguol.com.br/c/entretenimento/c4/2018/05/15/super-mario-odyssey-1526426783086_v2_1170x540.jpgx'
-                        title='Título'
-                        subtitle='Subtitle'
-                        content='Eu sou um content, estou sendo testado. Eu sou um content, estou sendo testado. Eu sou um content, estou sendo testado. '
-                        footer='Meu footer'
-                    />
-                    <Card
-                        img='https://conteudo.imguol.com.br/c/entretenimento/c4/2018/05/15/super-mario-odyssey-1526426783086_v2_1170x540.jpgx'
-                        title='Título'
-                        subtitle='Subtitle'
-                        content='Eu sou um content, estou sendo testado. Eu sou um content, estou sendo testado. Eu sou um content, estou sendo testado. '
-                        footer='Meu footer'
-                    />
-                    <Card
-                        img='https://conteudo.imguol.com.br/c/entretenimento/c4/2018/05/15/super-mario-odyssey-1526426783086_v2_1170x540.jpgx'
-                        title='Título'
-                        subtitle='Subtitle'
-                        content='Eu sou um content, estou sendo testado. Eu sou um content, estou sendo testado. Eu sou um content, estou sendo testado. '
-                        footer='Meu footer'
-                    />
-                    <Card
-                        img='https://conteudo.imguol.com.br/c/entretenimento/c4/2018/05/15/super-mario-odyssey-1526426783086_v2_1170x540.jpgx'
-                        title='Título'
-                        subtitle='Subtitle'
-                        content='Eu sou um content, estou sendo testado. Eu sou um content, estou sendo testado. Eu sou um content, estou sendo testado. '
-                        footer='Meu footer'
-                    />
-                    <Card
-                        img='https://conteudo.imguol.com.br/c/entretenimento/c4/2018/05/15/super-mario-odyssey-1526426783086_v2_1170x540.jpgx'
-                        title='Título'
-                        subtitle='Subtitle'
-                        content='Eu sou um content, estou sendo testado. Eu sou um content, estou sendo testado. Eu sou um content, estou sendo testado. '
-                        footer='Meu footer'
-                    />
-                </div>
-
-
-                <Table headers={['FirstName', 'Second Name', 'Third Name', 'Random', 'Food',]}>
-                    <TableRow values={['Polenta', 'com', 'ceva', 'mais', 'tarde?']} />
-                    <TableRow values={['4', '5', '7', '123', 'batata']} />
-                </Table>
-
-            </div>
-
-
-            <Footer
-                firstContent={`
+      <Footer
+        firstContent={`
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
                 et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
                 ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
                 Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             `}
-
-                secondContent={`
+        secondContent={`
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
             et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
             ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             `}
-            >
-
-                <FooterBottom content='Meu cu de paraquedas eu sou o copyright maneiro _|_' />
-            </Footer>
-        </div>
-    )
+      >
+        <FooterBottom content="Footer content" />
+      </Footer>
+    </div>
+  )
 }
--- src/lib/atomix.js
+++ src/lib/atomix.js
@@ -1,187 +1,166 @@
-import './styles/root.css';
-import './styles/navbar.css';
-import './styles/button.css';
-import './styles/input.css';
-import './styles/card.css';
-import './styles/table.css';
-import './styles/footer.css';
-import './styles/modal.css';
+import './styles/root.css'
+import './styles/navbar.css'
+import './styles/button.css'
+import './styles/input.css'
+import './styles/card.css'
+import './styles/table.css'
+import './styles/footer.css'
+import './styles/modal.css'
 
-import React, { Fragment } from 'react';
-
+import React, { Fragment } from 'react'
 
 export function Button(props) {
-    return (
-        <button
-            onClick={props.onClick}
-            className={`btn btn-${props.style || 'default'}`}
-        >
-            {props.text}
-        </button>
-    )
+  return (
+    <button
+      onClick={props.onClick}
+      className={`btn btn-${props.style || 'default'}`}
+    >
+      {props.text}
+    </button>
+  )
 }
 
 export function Input(props) {
-    return (
-        <input
-            type={`${props.type || 'text'}`}
-            placeholder={props.placeholder}
-        />
-    )
+  return (
+    <input type={`${props.type || 'text'}`} placeholder={props.placeholder} />
+  )
 }
 
 export function Textarea(props) {
-    return (
-        <textarea
-            type={`${props.type || 'text'}`}
-            placeholder={props.placeholder}
-        >
-        </textarea>
-    )
+  return (
+    <textarea
+      type={`${props.type || 'text'}`}
+      placeholder={props.placeholder}
+    ></textarea>
+  )
 }
 
-
 export function Navbar(props) {
-    return (
-        <div className="navbar">
-            <a><img className='navbar-icon' href={props.link || '/'} src={props.icon} /></a>
-            <a className='navbar-brand'>
-                {props.brand}
-            </a>
-            <div className="navbar-content">
-                {props.children}
-            </div>
-        </div>
-    )
+  return (
+    <div className="navbar">
+      <a>
+        <img
+          className="navbar-icon"
+          href={props.link || '/'}
+          src={props.icon}
+        />
+      </a>
+      <a className="navbar-brand">{props.brand}</a>
+      <div className="navbar-content">{props.children}</div>
+    </div>
+  )
 }
 
 export function NavItem(props) {
-    return (
-        <a
-            href={props.link || '/'}
-            target={props.blankTarget ? '_BLANK' : ''}
-            onClick={props.onClick}
-            className='navbar-link'
-        >
-            {props.text}
-        </a>
-    )
+  return (
+    <a
+      href={props.link || '/'}
+      target={props.blankTarget ? '_BLANK' : ''}
+      onClick={props.onClick}
+      className="navbar-link"
+    >
+      {props.text}
+    </a>
+  )
 }
 
 export function Select(props) {
-    return (
-        <select>
-            <option> - </option>
-            {props.children}
-        </select>
-    )
+  return (
+    <select>
+      <option> - </option>
+      {props.children}
+    </select>
+  )
 }
 
 export function SelectOption(props) {
-    return (
-        <option key={Math.random()} value={props.value}> {props.text}</option>
-    )
+  return (
+    <option key={Math.random()} value={props.value}>
+      {' '}
+      {props.text}
+    </option>
+  )
 }
 
 export function Card(props) {
-    return (
-        <div className='card'>
-            <div className='card-image'>
-                <img src={props.img} />
-            </div>
-            <div className='card-title'>
-                {props.title}
-            </div>
-            <div className='card-subtitle'>
-                {props.subtitle}
-            </div>
+  return (
+    <div className="card">
+      <div className="card-image">
+        <img src={props.img} />
+      </div>
+      <div className="card-title">{props.title}</div>
+      <div className="card-subtitle">{props.subtitle}</div>
 
-            <div className='card-content'>
-                {props.content}
-            </div>
-            <div className='card-footer'>
-                {props.footer}
-            </div>
-        </div>
-    )
+      <div className="card-content">{props.content}</div>
+      <div className="card-footer">{props.footer}</div>
+    </div>
+  )
 }
 
 export function Table(props) {
-    return (
-        <table>
-            <thead>
-                <tr className='table-head'>
-                    {props.headers.map(item => (
-                        <th key={Math.random()}>{item}</th>
-                    ))}
-                </tr>
-            </thead>
-            <tbody>
-                {props.children}
-            </tbody>
-
-        </table>
-    )
+  return (
+    <table>
+      <thead>
+        <tr className="table-head">
+          {props.headers.map((item) => (
+            <th key={Math.random()}>{item}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>{props.children}</tbody>
+    </table>
+  )
 }
 
-
 export function TableRow(props) {
-    return (
-        <Fragment>
-            <tr className='table-row'>
-                {props.values.map(item => (
-                    <td key={Math.random()}>{item}</td>
-                ))}
-            </tr>
-        </Fragment>
-    )
+  return (
+    <Fragment>
+      <tr className="table-row">
+        {props.values.map((item) => (
+          <td key={Math.random()}>{item}</td>
+        ))}
+      </tr>
+    </Fragment>
+  )
 }
 
 export function Footer(props) {
-    return (
-        <div className='footer'>
-            <div className='footer-top'>
-                <div>
-                    {props.firstContent}
-                </div>
-                <div>
-                    {props.secondContent}
-                </div>
-            </div>
-            {props.children}
-        </div>
-    )
+  return (
+    <div className="footer">
+      <div className="footer-top">
+        <div>{props.firstContent}</div>
+        <div>{props.secondContent}</div>
+      </div>
+      {props.children}
+    </div>
+  )
 }
 
 export function FooterBottom(props) {
-    return (
-        <div className='footer-bottom'>
-            {props.content}
-        </div>
-    )
+  return <div className="footer-bottom">{props.content}</div>
 }
 
-
 export function Modal(props) {
-    if (!props.name) {
-        console.error(`Tag "name" is required to modal work correctly`);
-        return (<Fragment></Fragment>)
-    } else {
-        return (
-            <Fragment>
-                <a className={`modal-btn`} href={`#${props.name}`}>{props.text}</a>
+  if (!props.name) {
+    console.error(`Tag "name" is required to modal work correctly`)
+    return <Fragment></Fragment>
+  } else {
+    return (
+      <Fragment>
+        <a className={`modal-btn`} href={`#${props.name}`}>
+          {props.text}
+        </a>
 
-                <div id={props.name} className="overlay">
-                    <div className="modal">
-                        <h2>{props.title}</h2>
-                        <a className="close" href="#">×</a>
-                        <div className="content">
-                            {props.content}
-                        </div>
-                    </div>
-                </div>
-            </Fragment>
-        )
-    }
-
+        <div id={props.name} className="overlay">
+          <div className="modal">
+            <h2>{props.title}</h2>
+            <a className="close" href="#">
+              ×
+            </a>
+            <div className="content">{props.content}</div>
+          </div>
+        </div>
+      </Fragment>
+    )
+  }
 }
